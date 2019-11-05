4faa450e1cf5b3d16a92c447fd5f8cd01ac78439
do_tha000 <do_tha000@CAM-LAB-23.bgsd.k12.wa.us>
2019-11-05 07:35:37 -0800

    New Save 3

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -8,38 +8,38 @@ public class PokemonBattle {
 	}
 	public static String battlestart() {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Another tainer is issuing a chellenge!");
-		System.out.println("Zebstrika appeared.");
+		System.out.println("Another tainer is issuing a chellenge!\n");
+		System.out.println("Zebstrika appeared.\n");
 		System.out.print("Which Pokemon do you choose? ");
 		String youchoose = input.nextLine();
-		System.out.println("You chose " + youchoose + "!");
-		System.out.println("It's a Pokemon battle between " + youchoose + " and Zebstrika!   Go!");
+		System.out.println("\nYou chose " + youchoose + "!\n");
+		System.out.println("It's a Pokemon battle between " + youchoose + " and Zebstrika!   Go!\n");
 		return youchoose;
 	}
 	public static void damage(String dmgPoke) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Zebstrika used Thunderbolt!\n");
-		System.out.println("Trainer, what are your " + dmgPoke + "'s stats?");
+		System.out.println("Trainer, what are your " + dmgPoke + "'s stats?\n");
 		System.out.print("Level: ");
 		int lvl = input.nextInt();
-		System.out.print("Attack: ");
+		System.out.print("\nAttack: ");
 		int atk = input.nextInt();
-		System.out.print("Defense: ");
+		System.out.print("\nDefense: ");
 		int def = input.nextInt();
-		System.out.print("Base: ");
+		System.out.print("\nBase: ");
 		int base = input.nextInt();
-		System.out.print("STAB: ");
+		System.out.print("\nSTAB: ");
 		int stab = input.nextInt();
-		System.out.print("HP: ");
+		System.out.print("\nHP: ");
 		int hp = input.nextInt();
 		double mod = (0.85 + (Math.random() * 0.15)) * stab;
-		double dmg = (int) ((((2 * lvl + 10) / 250) + ((atk/def) * base + 2)) * mod);
-		int finalHp = (int) (dmg - hp);
+		int dmg = (int) ((((2 * lvl + 10) / 250) + ((atk/def) * base + 2)) * mod);
+		hp = hp - dmg;
 		System.out.println(dmgPoke + " sustained " + dmg + " points damage.");
-		System.out.println("HP, after damage, are now " + finalHp);
-		statsTable(dmgPoke);
+		System.out.println("HP, after damage, are now " + hp);
+		statsTable(dmgPoke, lvl, atk, def, base, stab, hp);
 	}
-	public static void statsTable(String name) {
-		System.out.println("Name   " + name);
+	public static void statsTable(String statsPoke, int statsLvl, int statsAtk, int statsDef, int statsBase, int statsStab, int statsHp) {
+		System.out.println("Name   " + statsPoke);
 	}
 }
